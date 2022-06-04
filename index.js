f4119ec7ecefd6f60b503be7cd4965d957a7f17e
Anuragkumarsah <asah2307@gmail.com>
2022-06-04 10:06:10 +0530

    Add Button

--- index.js
+++ index.js
@@ -5,22 +5,29 @@ function getRandomIntInclusive(min, max) {
 }
 
 
-var randomNumber1 = getRandomIntInclusive(1, 6);
-var randomNumber2 = getRandomIntInclusive(1, 6);
+function Roll(){
+
+    var randomNumber1 = getRandomIntInclusive(1, 6);
+    var randomNumber2 = getRandomIntInclusive(1, 6);
 
 
-var src1 = String("assets/dice" + randomNumber1 + ".png");
-var src2 = String("assets/dice" + randomNumber2 + ".png");
+    var src1 = String("assets/dice" + randomNumber1 + ".png");
+    var src2 = String("assets/dice" + randomNumber2 + ".png");
 
 
-document.querySelectorAll("img")[0].setAttribute("src", src1);
-document.querySelectorAll("img")[1].setAttribute("src", src2);
+    document.querySelectorAll("img")[0].setAttribute("src", src1);
+    document.querySelectorAll("img")[1].setAttribute("src", src2);
 
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Player1 Wins!"
-} else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player2 Wins! 🚩"
-} else {
-    document.querySelector("h1").innerHTML = "🚩 Draw! 🚩"
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🚩 Player1 Wins!"
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player2 Wins! 🚩"
+    } else {
+        document.querySelector("h1").innerHTML = "🚩 Draw! 🚩"
+    }
+
 }
+
+document.querySelector("button").addEventListener("click", Roll);
+
